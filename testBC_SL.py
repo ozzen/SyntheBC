e177de955c01bfc15e105e2ae99423cb8d5cb97f
ozzen <shroy@cs.stonybrook.edu>
2022-06-11 15:05:40 -0400

    Update testBC_SL.py

--- testBC_SL.py
+++ testBC_SL.py
@@ -5,32 +5,34 @@ from keras.models import load_model
 model = load_model('Results/candidateBC.h5')
 
 # rtds constraints
-high_d = 0.69
-low_d = 0.68
-high_q = 1e-5
-low_q = -1e-5
-high_md = 0.39
-low_md = 0.38
-high_mq = 0.055
-low_mq = 0.052
-high_v_oq = 0.008
-low_v_oq = 0.007
+i_d_ref = 0.68
 v_ref = 0.48
-v_init = 0.01*v_ref
+high_d = i_d_ref + 0.05*i_d_ref
+low_d = i_d_ref - 0.05*i_d_ref
+high_q = 1e-3
+low_q = -1e-3
+high_md = 0.40
+low_md = 0.30
+high_mq = 0.055
+low_mq = 0.0
+v_unsafe1 = 0.36
+v_unsafe2 = 0.60
 v_fsc = 0.03*v_ref
+v_safe = 0.005*v_ref
 
+# generates initial configuration
 def D1(flag):
     i_d = np.random.uniform(low=low_d, high=high_d)
     i_q = np.random.uniform(low=low_q, high=high_q)
     i_od = np.random.uniform(low=low_d, high=high_d)
     i_oq = np.random.uniform(low=low_q, high=high_q)
     if flag % 2 == 0:
-        v_od = np.random.uniform(low=v_ref-v_fsc-v_init, high=v_ref-v_fsc)
+        v_od = np.random.uniform(low=v_unsafe1, high=v_ref-v_fsc)
     else:
-        v_od = np.random.uniform(low=v_ref+v_fsc, high=v_ref+v_fsc+v_init)
-    v_oq = np.random.uniform(low=low_v_oq, high=high_v_oq)
+        v_od = np.random.uniform(low=v_ref+v_fsc, high=v_unsafe2)
+    v_oq = np.random.uniform(low=low_q, high=high_q)
     i_ld = np.random.uniform(low=low_d, high=high_d)
-    i_lq = np.random.uniform(low=0, high=0)
+    i_lq = np.random.uniform(low=low_q, high=high_q)
     m_d = np.random.uniform(low=low_md, high=high_md)
     m_q = np.random.uniform(low=low_mq, high=high_mq)
 
@@ -43,10 +45,10 @@ def D2():
     i_q = np.random.uniform(low=low_q, high=high_q)
     i_od = np.random.uniform(low=low_d, high=high_d)
     i_oq = np.random.uniform(low=low_q, high=high_q)
-    v_od = np.random.uniform(low=v_ref-v_init, high=v_ref+v_init)
-    v_oq = np.random.uniform(low=low_v_oq, high=high_v_oq)
+    v_od = np.random.uniform(low=v_ref-v_fsc+v_safe, high=v_ref+v_fsc-v_safe)
+    v_oq = np.random.uniform(low=low_q, high=high_q)
     i_ld = np.random.uniform(low=low_d, high=high_d)
-    i_lq = np.random.uniform(low=0, high=0)
+    i_lq = np.random.uniform(low=low_q, high=high_q)
     m_d = np.random.uniform(low=low_md, high=high_md)
     m_q = np.random.uniform(low=low_mq, high=high_mq)
 
@@ -54,15 +56,18 @@ def D2():
     dataset = 2
     return state, dataset
 
-def D3():
+def D3(flag):
     i_d = np.random.uniform(low=low_d, high=high_d)
     i_q = np.random.uniform(low=low_q, high=high_q)
     i_od = np.random.uniform(low=low_d, high=high_d)
     i_oq = np.random.uniform(low=low_q, high=high_q)
-    v_od = np.random.uniform(low=v_ref-v_fsc, high=v_ref+v_fsc)
-    v_oq = np.random.uniform(low=low_v_oq, high=high_v_oq)
+    if flag % 2 == 0:
+        v_od = np.random.uniform(low=v_ref-v_fsc, high=v_ref-v_fsc+v_safe)
+    else:
+        v_od = np.random.uniform(low=v_ref+v_fsc-v_safe, high=v_ref+v_fsc)
+    v_oq = np.random.uniform(low=low_q, high=high_q)
     i_ld = np.random.uniform(low=low_d, high=high_d)
-    i_lq = np.random.uniform(low=0, high=0)
+    i_lq = np.random.uniform(low=low_q, high=high_q)
     m_d = np.random.uniform(low=low_md, high=high_md)
     m_q = np.random.uniform(low=low_mq, high=high_mq)
 
@@ -70,6 +75,7 @@ def D3():
     dataset = 3
     return state, dataset
 
+# U = -1
 for i in range(100):
     data = []
     state, dataset = D2()
@@ -80,6 +86,10 @@ for i in range(100):
     Z_test = z_test.values
     action = model.predict(Z_test)
     u = float(action[0])
+    print(u)
+    # if u > U and u < 0:
+    #     print(state)
+    #     U = u
     if dataset == 1:
         if u > 0:
             R = True
@@ -91,7 +101,7 @@ for i in range(100):
         else:
             R = False
     if dataset == 3:
-        if u <= 0 and u >= -0.001:
+        if u <= 0: #and u >= -0.001:
             R = True
         else:
             R = False
